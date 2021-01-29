af2040eb78aac38acc52a82a0fcae39b68b5d223
kiemutecodes <kiemutecodes@gmail.com>
2021-01-29 13:09:02 -0500

    feature/initial

--- assets/js/game.js
+++ assets/js/game.js
@@ -1,1 +1,11 @@
-window.alert("This is an alert! Javascript is running!")
+// this creates a function named "fight"
+function fight() {
+    window.alert("The Fight has begun!");}
+    var playerName = window.prompt("What is your Robot's name?");
+    console.log(playerName); 
+console.log("This logs a string, good for leaving yourself a message");
+//This will do math and log 20
+console.log(10 + 10);
+//what is this?
+console.log("Our robot's name is " + playerName);
+
