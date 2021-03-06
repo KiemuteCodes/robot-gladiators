556334e7076f351c592d21ec392d013e05e3fcc6
kiemutecodes <kiemutecodes@gmail.com>
2021-03-06 03:34:39 -0500

    shop function

--- assets/js/game.js
+++ assets/js/game.js
@@ -92,6 +92,15 @@ for (var i = 0; i < enemyNames.length; i++) {
 
     // pass the pickedEnemyName variable's value into the fight function, where it will assume the value of the enemyName parameter
     fight(pickedEnemyName);
+    // if player is still alive and we're not the last enemy in the array
+    if (playerHealth > 0 && i < enemyNames.length - 1) {
+      //ask if player wants to use the store before the next round
+      var storeConfirm = window.confirm("The fight is over, visit the store before the next round:");
+      //if yes, take them to the shop
+    if (storeConfirm) {
+      shop();
+    }
+    }
   }
   // if player isn't alive, stop the game
   else {
@@ -101,6 +110,49 @@ for (var i = 0; i < enemyNames.length; i++) {
 //Play again
 }
 endGame();
+}
+
+var shop = function(){
+  //ask the player what they would like to do
+  var shopOptionPrompt = window.prompt( 
+    "Would you like to REFILL your health, UPGRADE your attack, or LEAVE the Store? Please enter one: 'REFILL','UPGRADE', or 'LEAVE' to make a choice."
+  );
+  //Use switch to carry out action
+  switch (shopOptionPrompt) {
+    case "REFILL": //new case
+    case "refill":
+      if (playerMoney >=7) {
+      window.alert("Refilling player's health by 20 for 7 dollars.");
+      //increase player health and decrease money
+      playerHealth = playerHealth + 20;
+      playerMoney = playerMoney - 7;
+      }
+      else{
+        window.alert("You don't have enough money!");
+      }
+      break;
+      case "UPGRADE": //new case
+      case "upgrade":
+        if (playerMoney >=7) {
+        window.alert("Upgrading player's attack by 6 for 7 dollars.");
+        playerAttack = playerAttack + 6;
+        playerMoney = playerMoney - 7;
+        }
+        else {
+          window.alert("You don't jave enough money");
+        }
+        break;
+        case "LEAVE": //new case
+        case "leave":
+        window.alert("Leaving the store");
+        //do nothing so function will end
+        break;
+        default:
+          window.alert("You did not pick a valid option. Try again.");
+          //call shop () again to force player to pick a valid option
+          shop();
+          break;
+  }
 }
 
 startGame();
